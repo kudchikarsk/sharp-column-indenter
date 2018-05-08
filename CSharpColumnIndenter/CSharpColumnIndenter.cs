9019479c7a369c62e47ba131c3ac8669ef1aa1c8
Shadman Kudchikar <shadmanssk@yahoo.in>
2018-05-08 23:09:16 +0530

    Add project files.

---
+++ CSharpColumnIndenter/CSharpColumnIndenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CSharpColumnIndenter
+{
+    class CSharpColumnIndenter
+    {
+        public CSharpColumnIndenter()
+        {
+
+        }
+
+        public string Apply(string text)
+        {
+            SyntaxTree tree = SyntaxFactory.ParseSyntaxTree(text);
+
+            return text.ToUpper();
+        }
+    }
+}
